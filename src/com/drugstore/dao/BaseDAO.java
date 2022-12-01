b7147ad00c4cd50f251b2311b0761dcd1ab42969
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-01 20:11:22 +0800

    添加JDBC工具类以及通用的增删改查操作

---
+++ src/com/drugstore/dao/BaseDAO.java
@@ -0,0 +1,157 @@
+package com.drugstore.dao;
+
+import com.drugstore.utils.JDBCUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 周万宁
+ * @className BaseDAO
+ * @create 2022/12/1-19:42
+ * @descrTption 通用的增删改查操作
+ */
+public abstract class BaseDAO<T> {
+
+    private Class<T> clazz = null;
+
+
+//	public BaseDAO(){
+//
+//	}
+
+    {
+        //获取当前BaseDAO的子类继承的父类中的泛型
+        Type genericSuperclass = this.getClass().getGenericSuperclass();
+        ParameterizedType paramType = (ParameterizedType) genericSuperclass;
+
+        Type[] typeArguments = paramType.getActualTypeArguments();//获取了父类的泛型参数
+        clazz = (Class<T>) typeArguments[0];//泛型的第一个参数
+
+    }
+
+
+    // 通用的增删改操作---version 2.0 （考虑上事务）
+    public int update(Connection conn, String sql, Object... args) {// sql中占位符的个数与可变形参的长度相同！
+        PreparedStatement ps = null;
+        try {
+            // 1.预编译sql语句，返回PreparedStatement的实例
+            ps = conn.prepareStatement(sql);
+            // 2.填充占位符
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);// 小心参数声明错误！！
+            }
+            // 3.执行
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 4.资源的关闭
+            JDBCUtils.closeResource(null, ps);
+
+        }
+        return 0;
+
+    }
+
+    // 通用的查询操作，用于返回数据表中的一条记录（version 2.0：考虑上事务）
+    public T getInstance(Connection conn, String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据 :ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+
+            if (rs.next()) {
+                T t = clazz.newInstance();
+                // 处理结果集一行数据中的每一个列
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    // String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    // 给t对象指定的columnName属性，赋值为columValue：通过反射
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+
+        }
+
+        return null;
+    }
+
+    // 通用的查询操作，用于返回数据表中的多条记录构成的集合（version 2.0：考虑上事务）
+    public List<T> getForList(Connection conn, String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            // 获取结果集的元数据 :ResultSetMetaData
+            ResultSetMetaData rsmd = rs.getMetaData();
+            // 通过ResultSetMetaData获取结果集中的列数
+            int columnCount = rsmd.getColumnCount();
+            // 创建集合对象
+            ArrayList<T> list = new ArrayList<T>();
+            while (rs.next()) {
+                T t = clazz.newInstance();
+                // 处理结果集一行数据中的每一个列:给t对象指定的属性赋值
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取列值
+                    Object columValue = rs.getObject(i + 1);
+
+                    // 获取每个列的列名
+                    // String columnName = rsmd.getColumnName(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    // 给t对象指定的columnName属性，赋值为columValue：通过反射
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                list.add(t);
+            }
+
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+
+        }
+        return null;
+    }
+
+
+}
---
+++ src/com/drugstore/utils/JDBCUtils.java
@@ -0,0 +1,93 @@
+package com.drugstore.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * author 周万宁
+ * className JDBCUtils
+ * create 2022/12/1-19:57
+ * description JDBC工具类
+ */
+public class JDBCUtils {
+
+    public static void closeResource(Connection conn, Statement ps){
+        try{
+            if(ps != null)
+                ps.close();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        try {
+            if(conn != null)
+                conn.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void closeResource(Connection conn, Statement ps, ResultSet r){
+        try{
+            if(ps != null)
+                ps.close();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        try {
+            if(conn != null)
+                conn.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        try{
+            if(r!=null){
+                r.close();
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static DruidDataSource dataSource;
+    static {
+
+
+        try {
+            Properties properties = new Properties();
+            //读取jsbc.properties属性配置文件
+            InputStream is = JDBCUtils.class.getClassLoader().getResourceAsStream("jdbc.properties");
+            //从流中添加数据
+            properties.load(is);
+            //创建数据库连接池
+            dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //如果返回null说明获取联连接失败
+    public  static Connection getConnection()throws Exception{
+
+        Connection conn=null;
+
+            conn= dataSource.getConnection();
+
+        return conn;
+
+    }
+
+
+}
