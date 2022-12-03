b271d9647480178a9cc686630f6d411f6ee8fd3d
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-03 20:01:41 +0800

    2022.12.3 获取实例接口的编写

--- src/com/drugstore/service/getInstanceService.java
+++ src/com/drugstore/service/getInstanceService.java
@@ -1,8 +1,8 @@
 package com.drugstore.service;
 
-import com.drugstore.bean.CustomerInfo;
-import com.drugstore.bean.DrugInfo;
+import com.drugstore.bean.*;
 
+import java.sql.Connection;
 import java.util.List;
 
 /**
@@ -22,8 +22,75 @@ public interface getInstanceService {
      **/
     CustomerInfo getCustomerInfo(String phone);
 
+    /**
+     * @MethodName getByID
+     * @Author 周万宁
+     * @Description 根据id获取一个EmployeesInfo实例
+     * @Date 19:55 2022/12/3
+     * @Param [id]
+     * @return com.drugstore.bean.EmployeesInfo
+     **/
+    EmployeesInfo getEmployeesInfo(String id);
+
+    /**
+     * @MethodName getFinancialRevenueAndExpenditureInfo
+     * @Author 周万宁
+     * @Description 根据ID获取一个FinancialRevenueAndExpenditureInfo实例
+     * @Date 19:56 2022/12/3
+     * @Param [ID]
+     * @return com.drugstore.bean.FinancialRevenueAndExpenditureInfo
+     **/
+    FinancialRevenueAndExpenditureInfo getFinancialRevenueAndExpenditureInfo( String ID);
+
+    /**
+     * @MethodName getMarketingInfo
+     * @Author 周万宁
+     * @Description 根据ID获取一条MarketingInfo实例
+     * @Date 19:57 2022/12/3
+     * @Param [id]
+     * @return com.drugstore.bean.MarketingInfo
+     **/
+    MarketingInfo getMarketingInfo(String id);
+
+    /**
+     * @MethodName getOutboundInfo
+     * @Author 周万宁
+     * @Description 根据ID获取一条OutboundInfo实例
+     * @Date 19:58 2022/12/3
+     * @Param [id]
+     * @return com.drugstore.bean.OutboundInfo
+     **/
+    OutboundInfo getOutboundInfo(String id);
+
+    /**
+     * @MethodName getReturnInfo
+     * @Author 周万宁
+     * @Description 根据id获取ReturnInfo实例
+     * @Date 19:59 2022/12/3
+     * @Param [id]
+     * @return com.drugstore.bean.ReturnInfo
+     **/
+    ReturnInfo getReturnInfo (String id);
+
+    /**
+     * @MethodName getStorageEntryInfo
+     * @Author 周万宁
+     * @Description 根据ID获取StorageEntryInfo实例
+     * @Date 20:00 2022/12/3
+     * @Param [id]
+     * @return com.drugstore.bean.StorageEntryInfo
+     **/
+    StorageEntryInfo getStorageEntryInfo(String id);
 
 
-
+    /**
+     * @MethodName getSupplierInfo
+     * @Author 周万宁
+     * @Description 根据ID获取SupplierInfo实例
+     * @Date 20:00 2022/12/3
+     * @Param [id]
+     * @return com.drugstore.bean.SupplierInfo
+     **/
+    SupplierInfo getSupplierInfo(String id);
 
 }
