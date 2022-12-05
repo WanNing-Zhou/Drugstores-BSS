bec667a8d6b9b5e01db98f59877919f576ec22c8
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-05 11:59:08 +0800

    2022.12.4 编写service单例获取

---
+++ src/com/drugstore/factory/ServiceSingleton.java
@@ -0,0 +1,45 @@
+package com.drugstore.factory;
+
+import com.drugstore.service.*;
+import com.drugstore.service.impl.*;
+
+/**
+ * @author 周万宁
+ * @className ServiceSingleton
+ * @create 2022/12/5-11:52
+ * @description 单例模式获取service单例
+ */
+public class ServiceSingleton {
+
+    private static LoginService loginService= new LoginServiceImpl();
+    private static AddInfoService addInfoService = new AddInfoServiceImpl();
+    private static DeleteService deleteService = new DeleteServiceImpl();
+    private static GetInstanceService getInstanceService = new GetInstanceServiceImpl();
+    private static GetForListService getForListService = new GetForListServiceImpl();
+
+
+    //私有化构造方法
+    private  ServiceSingleton(){};
+
+
+
+    public static LoginService getLoginService() {
+        return loginService;
+    }
+
+    public static AddInfoService getAddInfoService() {
+        return addInfoService;
+    }
+
+    public static DeleteService getDeleteService() {
+        return deleteService;
+    }
+
+    public static GetInstanceService getGetInstanceService() {
+        return getInstanceService;
+    }
+
+    public static GetForListService getGetForListService() {
+        return getForListService;
+    }
+}
