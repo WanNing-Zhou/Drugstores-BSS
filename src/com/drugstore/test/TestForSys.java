fa54cb2b5b7a1da70b0e028c2c64c060287df58b
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-12-06 10:43:02 +0800

    2022.12.5 请求接口路径的第二次修改,过滤器修改

--- src/com/drugstore/test/TestForSys.java
+++ src/com/drugstore/test/TestForSys.java
@@ -1,5 +1,12 @@
 package com.drugstore.test;
 
+import com.drugstore.bean.EmployeesInfo;
+import com.drugstore.dao.EmployeesInfoDAO;
+import com.drugstore.factory.DAOSingleton;
+import com.drugstore.utils.JDBCUtils;
+
+import java.sql.Connection;
+
 /**
  * @author 周万宁
  * @className TestForSys
@@ -7,4 +14,27 @@ package com.drugstore.test;
  * @description 测试类
  */
 public class TestForSys {
+    public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            //获取数据库连接
+             conn = JDBCUtils.getConnection();
+             //通过单例工厂获取dao
+            EmployeesInfoDAO dao = DAOSingleton.getEmployeesInfoDAO();
+
+            //如果不知道方法的参数可以 ctrl + p 来获取参数提示
+            //使用dao对员工进行操作
+            //因为要添加,所以需要新建一个empl实例
+            EmployeesInfo empl = new EmployeesInfo("张三","123456","员工","123456");
+            dao.insert(conn,empl);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //记得要关闭数据库连接
+            JDBCUtils.closeResource(conn,null);
+        }
+    }
+
+
 }
